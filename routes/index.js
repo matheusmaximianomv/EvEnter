5c4430d759e70892ed5e0e33dec164f53f9e0939
matheusmaximianomv <44318510+matheusmaximianomv@users.noreply.github.com>
2019-05-20 21:19:37 -0300

    Delete index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-/* Rota Principal */
-const express = require("express");
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send("Primeira Página do Projeto");
-});
-
-module.exports = router;
