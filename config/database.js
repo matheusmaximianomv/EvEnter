798605d65d0b67698e0383bdc2f17f2e387d6fc0
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2019-06-17 19:43:46 -0300

    Finalizando todas as funções do controller de usuário

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect : "postgres",
-  port : 3024,
-  host : "labsql.fapce.edu.br",
-  schema : "web_poo",
-  database : "fap_2019_1",
-  username : "mbd_2018110268",
-  password : "matheus123456789",
+  port : 0000,
+  host : "",
+  schema : "",
+  database : "",
+  username : "",
+  password : "",
   logging : false
 }
