591ea7c3350e91b9c34a7bd72205ca365e35c224
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-17 19:03:43 -0300

    Cadastro categoria

--- app/controllers/CategorieController.js
+++ app/controllers/CategorieController.js
@@ -1,4 +1,4 @@
-const { Categorie } = require("../models/Categorie");
+const { Categorie } = require("../models");
 
 module.exports = {
   //Cadastrar categoria
@@ -12,11 +12,13 @@ module.exports = {
       });
 
     const newCategorie = { name };
+    console.log(newCategorie);
 
     try {
       const categorie = await Categorie.create(newCategorie);
       return res.json(categorie);
     } catch (err) {
+      //console.log(err);
       return res.json({
         error: "Erro ao Cadastrar",
         description: "Erro no Servidor.",
