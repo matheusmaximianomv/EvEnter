c4c4275737c0a286c810ab6ba266df1e9895adc9
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2019-06-17 18:57:49 -0300

    Alterações no Controller User

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: "postgres",
-  port: 3024,
-  host: "labsql.fapce.edu.br",
-  schema: "web_poo",
-  database: "fap_2019_1",
-  username: "mbd_2018110308",
-  password: "string18;",
+  port: 0000,
+  host: "",
+  schema: "",
+  database: "",
+  username: "",
+  password: "",
   logging: false
 };
