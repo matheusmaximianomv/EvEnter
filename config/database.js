9282097fb8ff1437551c46af934e62c5865bc9c2
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2019-06-17 19:15:59 -0300

    Criando a função de deletar usuario

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
-  dialect: "postgres",
-  port: 3024,
-  host: "labsql.fapce.edu.br",
-  schema: "web_poo",
-  database: "fap_2019_1",
-  username: "mbd_2018110268",
-  password: "matheus123456789",
-  logging: false
-};
+  dialect : "postgres",
+  port : 0000,
+  host : "",
+  schema : "",
+  database : "",
+  username : "",
+  password : "",
+  logging : false
+}
